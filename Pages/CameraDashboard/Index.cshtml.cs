f11a97907cc9517435a95dc0b8ee5ab1be55e237
Nandhini S <nandhini.s@axiscades.in>
2024-02-28 12:06:24 +0530

    web

---
+++ Pages/CameraDashboard/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebCam_Streaming.Pages.CameraDashboard
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
